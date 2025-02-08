ae840766f411380c62b9dbe177d4e7220fbaf5a7
Adnan Ashraf <af.adnan23@gmail.com>
2025-02-08 06:38:16 +0530

    ping pong be done

--- part1/dist/index.js
+++ part1/dist/index.js
@@ -7,5 +7,17 @@ const wss = new ws_1.WebSocketServer({ port: 8080 });
 //socket is nothing but just a connection for the websockets
 wss.on("connection", function (socket) {
     console.log("User Connected ");
-    socket.send("hii you are connected to the wb server");
+    socket.send("hii you are connected to the ping pong WebSocket Server");
+    socket.on('message', function message(text) {
+        const clientText = text.toString();
+        const wbText = "pong";
+        console.log(clientText);
+        console.log(wbText);
+        if (clientText == "ping") {
+            return socket.send("pong");
+        }
+        else {
+            return socket.send("Please Send 'ping' Messages");
+        }
+    });
 });
--- part1/src/index.ts
+++ part1/src/index.ts
@@ -4,10 +4,21 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port : 8080});
 
 
-//event handler
-//jb v ws pe new connection aaye sent it to the socket(i.e send into the  function)
-//socket is nothing but just a connection for the websockets
+
 wss.on("connection",function(socket){
     console.log("User Connected ");
-    socket.send("hii you are connected to the wb server");
+    socket.send("hii you are connected to the ping pong WebSocket Server");
+    
+    socket.on('message', function message(text) {
+        const clientText = text.toString();
+        const wbText = "pong";
+        console.log(clientText);
+        console.log(wbText);
+
+        if(clientText == "ping"){
+            return socket.send("pong");
+        }else{
+            return socket.send("Please Send 'ping' Messages");
+        }
+})
 })
